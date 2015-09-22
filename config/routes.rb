279c23364d1ec21f83bb02623398fa8b9f0e72ef
Morgan Miller <m.miller722@gmail.com>
2015-09-22 17:38:40 -0600

    add user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   resources :channels, only: [:show]
   resources :messages, only: [:create]
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 end
