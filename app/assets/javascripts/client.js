6ca6831819ab801cb46ef22b6a96ed86e11ec453
justin <justin.ryan.holmes@icloud.com>
2015-09-28 18:13:58 -0600

    Add socket subscribe and unsubscribe events to redis channels. Can now send messages on separate channels

--- app/assets/javascripts/client.js
+++ app/assets/javascripts/client.js
@@ -1,3 +1,18 @@
 var socket = io.connect("localhost:8080");
 
+socket.on("connect", function() {
+  socket.emit("subscribe", "flack:1");
+});
+
+$(document).ready(function(){
+  $("#1").click(function() {
+    socket.emit("unsubscribe", "flack:2");
+    socket.emit("subscribe", "flack:1");
+  });
+
+  $("#2").click(function() {
+    socket.emit("unsubscribe", "flack:1");
+    socket.emit("subscribe", "flack:2");
+  });
+});
 
